8e22f5fd7624f5f306599d3ca1b276ce89b3e421
nurullahsakar <45341907+nurullahsakar@users.noreply.github.com>
2023-01-10 22:37:11 +0000

    deuxieme commit

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
 
+  userEmail!: string;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -15,6 +18,10 @@ export class LandingPageComponent implements OnInit {
 
   onContinue(): void {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm){
+    console.log(form.value);
   }
 
 }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -77,4 +77,14 @@ export class FaceSnapsService {
     const faceSnap = this.getFaceSnapById(faceSnapId);
     snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
   }
+
+  addFaceSnap(formValue: { title: string, description: string, imageUrl: string, location?: string }): void {
+    const faceSnap = {
+      ...formValue,
+      createdDate: new Date(),
+      snaps: 0,
+      id: this.faceSnaps[this.faceSnaps.length-1].id + 1
+    }
+    this.faceSnaps.push(faceSnap);
+  }
 }
